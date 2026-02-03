9dfa64292d5f87355a2fd685c1cb3898b795ed63
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2026-02-03 15:07:36 +1300

    Add lang-specific qotd/random endpoints
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -1,6 +1,7 @@
 PANDOC_FORMAT := markdown+yaml_metadata_block+backtick_code_blocks+fenced_code_attributes+inline_notes+emoji-implicit_figures+shortcut_reference_links+spaced_reference_links+autolink_bare_uris-citations
 LANG_ENDPOINTS := $(patsubst data/quotes-%.json,%.json,$(wildcard data/quotes-*.json))
+RAND_ENDPOINTS := $(patsubst data/quotes-%.json,%/random.json,$(wildcard data/quotes-*.json)) $(patsubst data/quotes-%.json,%/qotd.json,$(wildcard data/quotes-*.json)) qotd.json random.json
 ARR_ENDPOINTS := $(AUTH_ENDPOINTS) $(LANG_ENDPOINTS)
-OBJ_ENDPOINTS := $(OID_ENDPOINTS) qotd.json random.json
+OBJ_ENDPOINTS := $(OID_ENDPOINTS) $(RAND_ENDPOINTS)
 ENDPOINTS := $(ARR_ENDPOINTS) $(OBJ_ENDPOINTS) authors.json
 
