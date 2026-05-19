1a1dd495e6c53c44db6072656bf4275a0729fd4e
norwd <norwd@noreply.codeberg.org>
2026-05-19 05:48:09 +0200

    Expand endpoint lists
    Signed-off-by: norwd <norwd@noreply.codeberg.org>

--- config.mk
+++ config.mk
@@ -3,7 +3,8 @@ LANG_ENDPOINTS := $(patsubst data/quotes-%.json,%.json,$(wildcard data/quotes-*.
 ARR_ENDPOINTS := $(AUTH_ENDPOINTS) $(LANG_ENDPOINTS)
 OBJ_ENDPOINTS := $(OID_ENDPOINTS) qotd.json random.json
 JSON_ENDPOINTS := $(ARR_ENDPOINTS) $(OBJ_ENDPOINTS) authors.json
-MARKDOWN_ENDPOINTS := $(JSON_ENDPOINTS:.json=.md)
-HTML_ENDPOINTS := $(JSON_ENDPOINTS:.json=.html)
-TXT_ENDPOINTS := $(JSON_ENDPOINTS:.json=.txt)
-ENDPOINTS := $(JSON_ENDPOINTS) $(MARKDOWN_ENDPOINTS) $(HTML_ENDPOINTS) $(TXT_ENDPOINTS)
+MARKDOWN_ENDPOINTS := $(JSON_ENDPOINTS:.json=.md) $(wildcard *.md */*.md */*/*.md)
+HTML_ENDPOINTS := $(MARKDOWN_ENDPOINTS:.md=.html) $(patsubst %.md,%/index.html,$(MARKDOWN_ENDPOINTS)) index.html
+TXT_ENDPOINTS := $(MARKDOWN_ENDPOINTS:.md=.txt) $(HTML_ENDPOINTS:.html=.txt)
+META_ENDPOINTS := robots.txt security.txt # sitemap.txt version.txt
+ENDPOINTS := $(JSON_ENDPOINTS) $(MARKDOWN_ENDPOINTS) $(HTML_ENDPOINTS) $(TXT_ENDPOINTS) $(META_ENDPOINTS)
